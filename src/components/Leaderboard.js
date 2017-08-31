9b503d143c0b316c48251225511ec68871ecd17e
Lofthur <steintdale@gmail.com>
2017-08-31 13:55:39 +0200

    added meta tag, and changed inner html on click

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -13,6 +13,18 @@ export class Leaderboard extends React.Component {
 	handleClick(e) {
 		const url = e.target.dataset.url;
 		this.props.updateShowList(url);
+
+		switch(e.target.id) {
+			case 'recent':
+				e.target.innerHTML = 'Points in last 30 days &#9662;';
+				document.getElementById('top').innerHTML = 'All time points';
+				break;
+
+			case 'top':
+				e.target.innerHTML = 'All time points &#9662;';
+				document.getElementById('recent').innerHTML = 'Points in last 30 days';
+				break;
+		}
 	}
 
 
@@ -27,8 +39,8 @@ export class Leaderboard extends React.Component {
 						<tr>
 							<th>#</th>
 							<th>Camper Name</th>
-							<th><a href="#" data-url="https://fcctop100.herokuapp.com/api/fccusers/top/recent" onClick={this.handleClick}>Points in last 30 days &#9662;</a></th>
-							<th><a href="#" data-url="https://fcctop100.herokuapp.com/api/fccusers/top/alltime" onClick={this.handleClick}>All time points</a></th>
+							<th><a href="#"  id="recent" data-url="https://fcctop100.herokuapp.com/api/fccusers/top/recent" onClick={this.handleClick}>Points in last 30 days &#9662;</a></th>
+							<th><a href="#" id="top" data-url="https://fcctop100.herokuapp.com/api/fccusers/top/alltime" onClick={this.handleClick}>All time points</a></th>
 						</tr>
 					</thead>	
 					<Campers list={this.props.list} />
